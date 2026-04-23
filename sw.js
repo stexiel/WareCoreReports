31586cfe6232d6e45df9b16bfc735a43bfe550ee
stexiel <stexielgithub@gmail.com>
2026-04-23 19:24:18 +0500

    Добавить PWA: manifest.json, Service Worker, кнопка установки приложения для работы как десктопное приложение

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'warecore-reports-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './Снимок_экрана_2026-04-19_043623-removebg-preview.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.0/exceljs.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
